490237d84dd277fbff02c7dc0c811cdcab43dcbb
mayaradel97 <77214336+mayaradel97@users.noreply.github.com>
2021-06-22 08:57:35 -0700

    Initial Commit

---
+++ Dice-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dice-SwiftUI
+//
+//  Created by Mayar Adel on 6/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
