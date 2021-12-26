f7dd56b52472cf1d9076c6138469455b83e96f1d
mayaradel97 <77214336+mayaradel97@users.noreply.github.com>
2021-12-26 10:59:31 -0800

    app

--- Dice-SwiftUI/ContentView.swift
+++ Dice-SwiftUI/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftDiceNumber = Int.random(in: 1...6)
+    @State var rightDiceNumber = Int.random(in: 1...6)
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       ZStack
+       {
+        Image("background")
+            .resizable()//take the remaining space
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("diceeLogo")
+            Spacer()
+            HStack {
+                DiceView(diceNumber: leftDiceNumber)
+                DiceView(diceNumber: rightDiceNumber)
+                
+            }
+            Spacer()
+            Button(action: {
+                [self ] in
+                leftDiceNumber = Int.random(in: 1...6)
+                rightDiceNumber = Int.random(in: 1...6)
+            }) {
+                Text("Roll")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+                    .font(.system(size: 50))
+                
+            }.background(Color.red)
+        }
+       }
     }
 }
-
+struct DiceView: View {
+    let diceNumber :Int
+    var body: some View {
+        Image("dice\(diceNumber)").resizable().aspectRatio(1, contentMode: .fit).padding()
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
